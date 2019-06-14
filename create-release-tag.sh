2b252cb1ad658f08e27af146126e5d97fc84b329
Sateesh Mathangi <a-smathangi@hotels.com>
2019-06-14 13:11:59 +0100

    missed file

--- create-release-tag.sh
+++ create-release-tag.sh
@@ -3,10 +3,14 @@
 git config --global user.email "sateeshkumar.m@gmail.com"
 git config --global user.name "smathangi"
 git fetch --tags
-
 export GIT_TAG=$(jq -r ".version" package.json)
+
+if [[ "$TRAVIS_PULL_REQUEST" == "true" && "$GIT_TAG" != "*-prerelease"]]; then
+    echo "PULL REQUEST must have a prerelease version to tag"
+    exit
+fi
 if git tag $GIT_TAG -a -m "Generated tag from TravisCI build $TRAVIS_BUILD_NUMBER" 2>/dev/null; then
     git push https://${GH_TOKEN}@github.com/covfefe-spikes/nodejs-release-spike.git --tags
   else
-    echo Tag already exists!
+    echo Tag $GIT_TAG already exists!
 fi
