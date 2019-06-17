677f1a87da9e4f201cd69f73b997d1cb3d2c2840
Sateesh Mathangi <a-smathangi@hotels.com>
2019-06-17 12:26:19 +0100

    released new version
    # Conflicts:
    #	package.json

--- create-release-tag.sh
+++ create-release-tag.sh
@@ -1,19 +1,21 @@
 #!/usr/bin/env bash
 
 createTag() {
-  if git tag v$GIT_TAG -a -m "Generated tag from TravisCI build $TRAVIS_BUILD_NUMBER" 2>/dev/null; then
-    git push https://${GH_TOKEN}@github.com/covfefe-spikes/nodejs-release-spike.git --tags
-  else
-    echo Tag v$GIT_TAG already exists!;
-  fi
+    git ls-remote --tags origin $TAG_NAME 2>/dev/null | grep $TAG_NAME 1>/dev/null
+    if [ "$?" == 0 ]; then
+       echo Tag $TAG_NAME already exists!;
+    else
+       git tag $TAG_NAME -a -m "Generated tag from TravisCI build $TRAVIS_BUILD_NUMBER"
+       git push https://${GH_TOKEN}@github.com/covfefe-spikes/nodejs-release-spike.git --tags >/dev/null
+    fi
 }
 
 git config --global user.email "sateeshkumar.m@gmail.com"
 git config --global user.name "smathangi"
-git fetch --tags
-export GIT_TAG=$(jq -r ".version" package.json)
+export PACKAGE_VERSION=$(jq -r ".version" package.json)
+export TAG_NAME="v$PACKAGE_VERSION"
 
-if [[ "$TRAVIS_PULL_REQUEST" != "false" && "$GIT_TAG" != *"-prerelease" ]]; then
+if [[ "$TRAVIS_PULL_REQUEST" != "false" && "$PACKAGE_VERSION" != *"-prerelease" ]]; then
       echo PR branch must have a prerelease version to tag
     else
      createTag
